6208ea6e5ca4a1f222ec1772e2f9ad83345d9b69
dpsiu <denversiu24@gmail.com>
2023-04-15 00:08:10 -0500

    Style and create variable grid layout

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ document.addEventListener("DOMContentLoaded", function(){
     console.log("hi");
 })
 
+let newGrid = document.querySelector('#newGrid');
+newGrid.addEventListener("click", function(){
+    let size = getSize();
+    createGrid(size);
+});
+
 function createGrid(size){
     let grid = document.getElementById("grid");
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -15,5 +21,22 @@ function createGrid(size){
         div.style.backgroundColor = "aliceblue";
         grid.insertAdjacentElement("beforeend", div);
     }
+
+    
 }
 
+function getSize(){
+    let size = prompt("How many rows/columns would you like?");
+    if (size == ''){
+        alert("Please enter a number between 0 & 100.")
+    }
+    else if (size < 0){
+        alert("Please enter a number greater than 0 and less than 100.");
+    }
+    else if (size > 100){
+        alert("Please enter a number less than 100.");
+    }
+    else {
+        return size;
+    }
+}
